074600d013bb582b8603af9121deec5ca4ab2318
sean4101 <seanchen1014@gmail.com>
2020-09-18 21:18:34 +0800

    Added a test model python file. Adjusted and deleted some unnecessary methods in widget and env file.

---
+++ Test_RL_Model.py
@@ -0,0 +1,18 @@
+
+
+class test_model():
+
+    def __init__(self, action_space, observation_space):
+        self.action_space = action_space
+        self.observation_space = observation_space
+
+        self.buffer = None
+
+    def predict(self, state):
+        ''' Returns a action based on given state. '''
+
+        return 0
+
+    def add_transition(transition):
+
+        self.buffer = None
--- Traffic_Simulator_Main.py
+++ Traffic_Simulator_Main.py
@@ -5,21 +5,29 @@ from PyQt5.QtWidgets import QApplication
 
 from Traffic_Simulator_Environment import Traffic_Simulator_Env
 from Traffic_Simulator_Widget import mainWidget
+from Test_RL_Model import test_model
 
 
 class Traffic_Simulator():
 
     def __init__(self):
+
+        self.model = test_model(3, 3)
         self.env = Traffic_Simulator_Env()
-        self.widget = mainWidget(self.env)
+        self.widget = mainWidget()
+
         self.env.setView(self.widget.ViewTab)
 
         self.view = self.widget.ViewTab
         self.train = self.widget.trainGroup
 
-    #def assignButtons(self):
+        self.assignButtons()
 
-    # Hello
+    def assignButtons(self):
+        self.train.stepButton.clicked.connect(self.envStep)
+
+    def envStep(self):
+        self.env.step(1)
         
 
 if __name__ == '__main__':
